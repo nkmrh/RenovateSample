eff42cc06de5554ceaf06741fa42efc3c980ae63
nkmrh <hajime.nakamura@quipper.com>
2022-07-13 19:36:24 +0900

    Add xcode project

---
+++ RenovateSample/RenovateSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenovateSampleApp.swift
+//  RenovateSample
+//
+//  Created by hajime-nakamura on 2022/07/13.
+//
+
+import SwiftUI
+
+@main
+struct RenovateSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
